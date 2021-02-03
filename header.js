d0928ac8a277e7f560d206ac2e90408dd5a5037f
Fabian Peña <fabiancpl91@gmail.com>
2021-02-02 22:14:04 -0500

    First version of the website

---
+++ header.js
@@ -0,0 +1,35 @@
+document.write('\
+\
+<!-- Navbar -->\
+<nav class="navbar navbar-default navbar-fixed-top">\
+  <div class="container">\
+    <div class="navbar-header">\
+      <a class="navbar-brand brand" href="index.html">WebDev</a>\
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">\
+        <span class="icon-bar"></span>\
+        <span class="icon-bar"></span>\
+        <span class="icon-bar"></span>\
+      </button>\
+    </div>\
+\
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
+\
+      <ul class="nav navbar-nav navbar-right">\
+        <li><a href="index.html#overview">Contenido</a></li>\
+        <li><a href="syllabus.html">Programa</a></li>\
+        <li><a href="index.html#faq">FAQ</a></li>\
+      </ul>\
+    </div>\
+  </div>\
+</nav>\
+\
+<!-- Header -->\
+<div id="header" style="text-align:center">\
+  <a href="https://www.unbosque.edu.co/">\
+    <img src="images/elbosque-logo.jpg" class="logo-right">\
+  </a>\
+  <h1>Web Development</h1>\
+  <h3>Universidad El Bosque, 2021</h3>\
+  <div style="clear:both;"></div>\
+</div>\
+');
